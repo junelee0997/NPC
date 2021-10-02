8d95e5fc7e950b8cacf69820aa093e05f5332bb4
junelee0997 <hclee0945@gmail.com>
2021-10-02 12:57:19 +0900

    frame

---
+++ backend/routes/admin.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+router.post('/login', function(req, res, next) {
+  
+});
+router.post('/login', function(req, res, next) {
+  
+});
+//사용자:로그인(post), 회원가입(post), 사진 전송(post), 코인 정보 받기(get), 
+//관리자:로그인(post), 회원가입(post), 사진 받기(get), 코인 지급(put), 
+module.exports = router;
